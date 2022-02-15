875ca8291c69b1eec30624051f682ef3e83058ad
Jaeho Jung <iwogh3176@gmail.com>
2022-02-15 23:26:41 +0900

    1234678

--- static/js/main.js
+++ static/js/main.js
@@ -276,6 +276,6 @@ function getFormatDate(date) {
 
 // 로그인_parunson-최신상태_수
 function loginCheck() {
-    const userId = $('#userId').val() //아이디 chu_123
+    const userId = $('#userId').val() //아이디 chu_1234
     const userPassword = $('#userPassword').val() //패스워드
 }
